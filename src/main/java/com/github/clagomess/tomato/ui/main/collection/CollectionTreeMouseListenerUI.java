00f32cacf65e5ebe1d89c2ad2c1bbf4616393572
Cláudio Gomes <cla.gomess@gmail.com>
2024-12-08 21:43:38 -0300

    add pop up menu

--- src/main/java/com/github/clagomess/tomato/ui/main/collection/CollectionTreeMouseListenerUI.java
+++ src/main/java/com/github/clagomess/tomato/ui/main/collection/CollectionTreeMouseListenerUI.java
@@ -1,7 +1,6 @@
 package com.github.clagomess.tomato.ui.main.collection;
 
 import com.github.clagomess.tomato.dto.CollectionTreeDto;
-import com.github.clagomess.tomato.dto.RequestDto;
 import com.github.clagomess.tomato.publisher.RequestPublisher;
 import lombok.RequiredArgsConstructor;
 
@@ -17,15 +16,57 @@ public class CollectionTreeMouseListenerUI extends MouseAdapter {
     private final RequestPublisher requestPublisher = RequestPublisher.getInstance();
 
     @Override
-    public void mousePressed(MouseEvent e) {
+    public void mouseReleased(MouseEvent e) {
         int selRow = tree.getRowForLocation(e.getX(), e.getY());
         TreePath selPath = tree.getPathForLocation(e.getX(), e.getY());
-        if (selRow != -1 && e.getClickCount() == 2 && selPath != null) {
-            DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) selPath.getLastPathComponent();
 
-            if(selectedNode.getUserObject() instanceof CollectionTreeDto.Request dto){
-                requestPublisher.getOnLoad().publish(dto);
-            }
+        if(selRow == -1 || selPath == null) return;
+
+        DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) selPath.getLastPathComponent();
+
+        if (e.getButton() == 1 && e.getClickCount() == 2 &&
+                selectedNode.getUserObject() instanceof CollectionTreeDto.Request dto) {
+            requestPublisher.getOnLoad().publish(dto);
+            return;
         }
+
+        if(e.getButton() == 3 &&
+                selectedNode.getUserObject() instanceof CollectionTreeDto.Request dto){
+            showRequestPopUpMenu(e, dto);
+            return;
+        }
+
+        if(e.getButton() == 3 &&
+                selectedNode.getUserObject() instanceof CollectionTreeDto dto){
+            showCollectionPopUpMenu(e, dto);
+            return;
+        }
+    }
+
+    private void showRequestPopUpMenu(
+            MouseEvent e,
+            CollectionTreeDto.Request dto
+    ){
+        JPopupMenu popup = new JPopupMenu();
+        popup.add(new JMenuItem("Open"){{
+            addActionListener(e -> requestPublisher.getOnLoad().publish(dto));
+        }});
+        popup.add(new JMenuItem("Move"));
+        popup.add(new JMenuItem("Rename"));
+        popup.addSeparator();
+        popup.add(new JMenuItem("Delete"));
+        popup.show(e.getComponent(), e.getX(), e.getY());
+    }
+
+    private void showCollectionPopUpMenu(
+            MouseEvent e,
+            CollectionTreeDto dto
+    ){
+        JPopupMenu popup = new JPopupMenu();
+        popup.add(new JMenuItem("Move"));
+        popup.add(new JMenuItem("Rename"));
+        popup.addSeparator();
+        popup.add(new JMenuItem("Delete"));
+        popup.show(e.getComponent(), e.getX(), e.getY());
     }
 }
