62ee0e4dbc9f79567bb950a94bb3d307257ca34c
Cláudio Gomes <cla.gomess@gmail.com>
2024-12-21 14:58:07 -0300

    jersey client -> vanilla

--- src/main/java/com/github/clagomess/tomato/service/HttpService.java
+++ src/main/java/com/github/clagomess/tomato/service/HttpService.java
@@ -4,122 +4,76 @@ import com.github.clagomess.tomato.dto.ResponseDto;
 import com.github.clagomess.tomato.dto.data.RequestDto;
 import com.github.clagomess.tomato.mapper.RequestMapper;
 import com.github.clagomess.tomato.util.HttpLogCollectorUtil;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.client.Invocation;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.ssl.SSLContexts;
-import org.glassfish.jersey.client.ClientConfig;
-import org.glassfish.jersey.client.ClientProperties;
-import org.glassfish.jersey.logging.LoggingFeature;
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
-import javax.net.ssl.SSLContext;
-import java.io.File;
-import java.util.logging.Level;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.util.List;
+
+import static com.github.clagomess.tomato.enums.HttpMethodEnum.POST;
+import static com.github.clagomess.tomato.enums.HttpMethodEnum.PUT;
 
 @Slf4j
 public class HttpService {
     @Getter
     private static final HttpService instance = new HttpService();
+    private HttpService() {}
 
-    private final RequestMapper mapper = RequestMapper.INSTANCE;
-
-    private final Client client;
+    private final RequestMapper mapper = RequestMapper.INSTANCE; //@TODO: check possible unused methods
     private final HttpLogCollectorUtil httpLogCollectorUtil = new HttpLogCollectorUtil();
-    private HttpService() {
-        ClientConfig config = new ClientConfig();
-        config.property(ClientProperties.FOLLOW_REDIRECTS, true);
-        config.property(ClientProperties.CONNECT_TIMEOUT, 1000 * 10);
-        config.register(MultiPartFeature.class);
-        config.register(new LoggingFeature(
-                httpLogCollectorUtil,
-                Level.INFO,
-                LoggingFeature.Verbosity.PAYLOAD_ANY,
-                600
-        ));
 
-//        if(restParam.getProxy() != null){ //@TODO: impl. use of proxy
-//            config.connectorProvider(new ApacheConnectorProvider());
-//            config.property(ClientProperties.PROXY_URI, restParam.getProxy().getUri());
-//            config.property(ClientProperties.PROXY_USERNAME, restParam.getProxy().getUsername());
-//            config.property(ClientProperties.PROXY_PASSWORD, restParam.getProxy().getPassword());
-//        }
-
-        ClientBuilder clientBuilder = ClientBuilder.newBuilder();
-        clientBuilder.withConfig(config);
-        clientBuilder.sslContext(getSslContext());
-
-        client = clientBuilder.build();
-    }
-
-    private static SSLContext getSslContext() {
-        try {
-            return SSLContexts.custom().loadTrustMaterial(
-                    null,
-                    (TrustStrategy) (x509Certificates, authType) -> true
-            ).build();
-        }catch (Exception e){
-            throw new RuntimeException(e.getMessage(), e);
-        }
-    }
-
-    private Entity<?> buildEntity(RequestDto.Body body){
-        return switch (body.getType()) {
-            case URL_ENCODED_FORM -> Entity.form(
-                    mapper.toForm(body.getUrlEncodedForm())
-            );
-            case MULTIPART_FORM -> Entity.entity(
-                    mapper.toFormDataMultiPart(body.getMultiPartForm()),
-                    MediaType.MULTIPART_FORM_DATA_TYPE
-            );
-            case RAW -> Entity.entity(
-                    body.getRaw().getRaw(),
-                    body.getRaw().getType().getContentType()
-            );
-            case BINARY -> Entity.entity(
-                    new File(body.getBinary().getFile()),
-                    body.getBinary().getContentType()
-            );
-            default -> Entity.text(null);
-        };
+    private HttpClient getClient() {
+        // @TODO: check ssl
+        // @TODO: FOLLOW_REDIRECTS
+        // @TODO: TIMEOUT
+        return HttpClient.newHttpClient();
     }
 
     public ResponseDto perform(RequestDto dto){
         ResponseDto result = new ResponseDto(dto.getId());
 
         try {
-            Invocation.Builder invocationBuilder = client.target(dto.getUrl()).request();
+            HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+                    .uri(URI.create(dto.getUrl()));
 
             // set headers
-            invocationBuilder.headers(mapper.toMultivaluedMap(dto.getHeaders()));
+            dto.getHeaders().forEach(header -> requestBuilder.header(
+                    header.getKey(),
+                    header.getValue()
+            ));
 
             // set cookies
-            dto.getCookies().forEach(item -> invocationBuilder.cookie(item.getKey(), item.getValue()));
+//            dto.getCookies().forEach(item -> invocationBuilder.cookie(item.getKey(), item.getValue())); //@TODO: impl. send cookie
 
+            HttpRequest request = buildBody(requestBuilder, dto);
             httpLogCollectorUtil.flush();
+
             long requestTime = System.currentTimeMillis();
 
-            try(Response response = performRequest(invocationBuilder, dto)){
-                var resultHttp = new ResponseDto.Response();
-                resultHttp.setRequestTime(System.currentTimeMillis() - requestTime);
+            HttpResponse<byte[]> response = getClient().send(
+                    request,
+                    HttpResponse.BodyHandlers.ofByteArray()
+            );
 
-                resultHttp.setBody(response.readEntity(byte[].class));
-                resultHttp.setBodySize(resultHttp.getBody().length);
-                resultHttp.setStatus(response.getStatus());
-                resultHttp.setStatusReason(response.getStatusInfo().getReasonPhrase());
-                resultHttp.setHeaders(response.getStringHeaders());
-                resultHttp.setCookies(response.getCookies());
-                resultHttp.setContentType(response.getMediaType());
+            var resultHttp = new ResponseDto.Response();
+            resultHttp.setRequestTime(System.currentTimeMillis() - requestTime);
 
-                result.setRequestStatus(true);
-                result.setHttpResponse(resultHttp);
-            }
+            resultHttp.setBody(response.body());
+            resultHttp.setBodySize(resultHttp.getBody().length);
+            resultHttp.setStatus(response.statusCode());
+            resultHttp.setStatusReason("FOO");
+            resultHttp.setHeaders(response.headers().map());
+//            resultHttp.setCookies(response.getCookies()); //@TODO: impl. parse cookies?
+            resultHttp.setContentType(MediaType.TEXT_PLAIN_TYPE); //@TODO: impl. response content-type
+
+            result.setRequestStatus(true);
+            result.setHttpResponse(resultHttp);
         } catch (Throwable e) {
             result.setRequestMessage(e.getMessage());
             log.error(log.getName(), e);
@@ -130,13 +84,89 @@ public class HttpService {
         return result;
     }
 
-    private Response performRequest(Invocation.Builder invocationBuilder, RequestDto dto){
-        // @TODO: when buildEntity (URLENCODED, FORMDATA), remove declared Content-Type
-        return switch (dto.getMethod()) {
-            case POST -> invocationBuilder.post(buildEntity(dto.getBody()));
-            case PUT -> invocationBuilder.put(buildEntity(dto.getBody()));
-            case DELETE -> invocationBuilder.delete();
-            default -> invocationBuilder.get();
+    private HttpRequest buildBody(
+            HttpRequest.Builder httpRequestBuilder,
+            RequestDto dto
+    ) throws FileNotFoundException {
+        if(!List.of(PUT, POST).contains(dto.getMethod())){
+            return buildBodyEmpty(httpRequestBuilder, dto);
+        }
+
+        return switch (dto.getBody().getType()){
+            case RAW -> buildBodyRaw(httpRequestBuilder, dto);
+            case BINARY -> buildBodyBinary(httpRequestBuilder, dto);
+            case URL_ENCODED_FORM -> buildBodyUrlEncoded(httpRequestBuilder, dto);
+            case MULTIPART_FORM -> buildBodyMultipart(httpRequestBuilder, dto);
+            default -> buildBodyEmpty(httpRequestBuilder, dto);
         };
+    }
+
+    private HttpRequest buildBodyEmpty(
+            HttpRequest.Builder httpRequestBuilder,
+            RequestDto dto
+    ){
+        httpRequestBuilder.method(
+                dto.getMethod().getMethod(),
+                HttpRequest.BodyPublishers.noBody()
+        );
+
+        return httpRequestBuilder.build();
+    }
+
+    private HttpRequest buildBodyRaw(
+            HttpRequest.Builder httpRequestBuilder,
+            RequestDto dto
+    ){
+        httpRequestBuilder.header(
+                "Content-Type",
+                dto.getBody()
+                        .getRaw()
+                        .getType()
+                        .getContentType().toString()
+        );
+
+        httpRequestBuilder.method(
+                dto.getMethod().getMethod(),
+                HttpRequest.BodyPublishers.ofString(
+                        dto.getBody().getRaw().getRaw()
+                )
+        );
+
+        return httpRequestBuilder.build();
+    }
+
+    private HttpRequest buildBodyBinary(
+            HttpRequest.Builder httpRequestBuilder,
+            RequestDto dto
+    ) throws FileNotFoundException {
+        httpRequestBuilder.header(
+                "Content-Type",
+                dto.getBody()
+                        .getBinary()
+                        .getContentType()
+        );
+
+        httpRequestBuilder.method(
+                dto.getMethod().getMethod(),
+                HttpRequest.BodyPublishers.ofFile(
+                        Path.of(dto.getBody().getBinary().getFile())
+                )
+        );
+
+        return httpRequestBuilder.build();
+    }
+
+    private HttpRequest buildBodyUrlEncoded(
+            HttpRequest.Builder httpRequestBuilder,
+            RequestDto dto
+    ){
+        throw new RuntimeException("Not implemented yet"); //@TODO: impl URL_ENCODED
+    }
+
+    private HttpRequest buildBodyMultipart(
+            HttpRequest.Builder httpRequestBuilder,
+            RequestDto dto
+    ){
+        throw new RuntimeException("Not implemented yet"); //@TODO: impl URL_ENCODED
     }
 }
