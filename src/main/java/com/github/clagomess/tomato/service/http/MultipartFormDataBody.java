85ab28a997b278d4e7356c74a8863c6669490b3d
Cláudio Gomes <cla.gomess@gmail.com>
2025-01-08 11:34:37 -0300

    refactor MultipartFormDataBody

--- src/main/java/com/github/clagomess/tomato/service/http/MultipartFormDataBody.java
+++ src/main/java/com/github/clagomess/tomato/service/http/MultipartFormDataBody.java
@@ -34,35 +34,11 @@ public class MultipartFormDataBody {
                 writer.write(String.format("--%s\r\n", boundary));
 
                 if(item.getType() == TEXT){
-                    writer.write("Content-Type: text/plain\r\n");
-                    writer.write(String.format(
-                            "Content-Disposition: form-data; name=\"%s\"\r\n",
-                            item.getKey()
-                    ));
-                    writer.write("\r\n");
-                    // @TODO: impl. env inject
-                    if(item.getValue() != null) writer.write(item.getValue());
+                    writeTextBoundary(writer, item.getKey(), item.getValue());
                 }
 
                 if(item.getType() == FILE){
-                    if(item.getValue() == null) throw new FileNotFoundException(item.getKey());
-
-                    var itemFile = new File(item.getValue());
-                    writer.write("Content-Type: application/octet-stream\r\n");
-                    writer.write(String.format(
-                            "Content-Disposition: form-data; name=\"%s\"; filename=\"%s\"\r\n",
-                            item.getKey(),
-                            itemFile.getName()
-                    ));
-                    writer.write("\r\n");
-                    try(BufferedReader reader = new BufferedReader(new FileReader(itemFile))){
-                        char[] buffer = new char[8192];
-                        int n;
-
-                        while ((n = reader.read(buffer)) != -1) {
-                            writer.write(buffer, 0, n);
-                        }
-                    }
+                    writeFileBoundary(writer, item.getKey(), item.getValue());
                 }
 
                 writer.write("\r\n");
@@ -72,5 +48,45 @@ public class MultipartFormDataBody {
         }
 
         return file;
+    }
+
+    protected void writeTextBoundary(
+            Writer writer,
+            String key,
+            String value
+    ) throws IOException {
+        writer.write("Content-Type: text/plain\r\n");
+        writer.write(String.format(
+                "Content-Disposition: form-data; name=\"%s\"\r\n",
+                key
+        ));
+        writer.write("\r\n");
+        // @TODO: impl. env inject
+        if(value != null) writer.write(value);
+    }
+
+    protected void writeFileBoundary(
+            Writer writer,
+            String key,
+            String value
+    ) throws IOException {
+        if(value == null) throw new FileNotFoundException(key);
+
+        var itemFile = new File(value);
+        writer.write("Content-Type: application/octet-stream\r\n");
+        writer.write(String.format(
+                "Content-Disposition: form-data; name=\"%s\"; filename=\"%s\"\r\n",
+                key,
+                itemFile.getName()
+        ));
+        writer.write("\r\n");
+        try(BufferedReader reader = new BufferedReader(new FileReader(itemFile))){
+            char[] buffer = new char[8192];
+            int n;
+
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        }
     }
 }
