2230a816a2c0a9416b1c82a7adddd59581254b70
Cláudio Gomes <cla.gomess@gmail.com>
2025-01-17 02:20:12 -0300

    fix line break

--- src/main/java/com/github/clagomess/tomato/ui/component/envtextfield/EnvTextField.java
+++ src/main/java/com/github/clagomess/tomato/ui/component/envtextfield/EnvTextField.java
@@ -4,10 +4,8 @@ import com.github.clagomess.tomato.ui.component.svgicon.boxicons.BxListPlusIcon;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
-
-import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
-import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER;
 
 public class EnvTextField extends JPanel {
     private final EnvDocumentListener envDocumentListener;
@@ -19,15 +17,15 @@ public class EnvTextField extends JPanel {
         this.btnEnvView.setToolTipText("View Injected Environment");
         this.btnEnvView.addActionListener(e -> btnEnvViewAction());
 
-        this.textPane = new JTextPane(); // @TODO: disable render lne break
+        this.textPane = new JTextPane();
 
         this.envDocumentListener = new EnvDocumentListener(this.textPane.getStyledDocument());
         this.textPane.getStyledDocument().addDocumentListener(this.envDocumentListener);
         this.envDocumentListener.getOnChangeList().add(content -> setBtnEnvViewEnabledOrDisabled());
 
-        var spTextPane = new JScrollPane(textPane);
-        spTextPane.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_NEVER);
-        spTextPane.setHorizontalScrollBarPolicy(HORIZONTAL_SCROLLBAR_NEVER);
+        var noWrapPanel = new JPanel(new BorderLayout());
+        noWrapPanel.add(textPane);
+        var spTextPane = new JScrollPane(noWrapPanel);
 
         setLayout(new MigLayout(
                 "insets 0 0 0 0",
