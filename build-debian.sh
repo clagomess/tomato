5a0339ea90809ccde1a55e5d811ba2f6e348ceb8
Cláudio Gomes <cla.gomess@gmail.com>
2025-01-23 23:52:19 -0300

    wip impl build debian

---
+++ build-debian.sh
@@ -0,0 +1,32 @@
+rm -rf /tmp/tomato
+mkdir -p /tmp/tomato
+
+# base dirs
+mkdir -p /tmp/tomato/DEBIAN
+mkdir -p /tmp/tomato/usr/share/tomato
+mkdir -p /tmp/tomato/usr/bin
+
+# copy release
+cp -R ./target/release/** /tmp/tomato/usr/share/tomato
+
+# copy executable
+cp ./build-debian/tomato.sh /tmp/tomato/usr/bin/tomato
+chmod +x /tmp/tomato/usr/bin/tomato
+
+# copy icons
+mkdir -p /tmp/tomato/usr/share/icons/hicolor/64x64/apps
+cp ./build-debian/icon-64x64.png /tmp/tomato/usr/share/icons/hicolor/64x64/apps/tomato.png
+
+# copy desktop entry
+mkdir -p /tmp/tomato/usr/share/applications
+cp ./build-debian/.desktop /tmp/tomato/usr/share/applications/tomato.desktop
+
+# copy control
+cp ./build-debian/control /tmp/tomato/DEBIAN/
+chmod -R 0755 /tmp/tomato/DEBIAN
+
+# build
+dpkg-deb --build /tmp/tomato
+
+# install (Test Only)
+# sudo dpkg -i /tmp/tomato.deb
---
+++ build-debian/tomato.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+java -jar -splash:/usr/share/tomato/splash.png \
+  -Dfile.encoding=UTF-8 \
+  /usr/share/tomato/tomato-0.0.4.jar
