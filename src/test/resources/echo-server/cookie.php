9d3216c2826063098f7c1f779c5dc6dad6b989b5
Cláudio Gomes <cla.gomess@gmail.com>
2025-02-06 17:25:01 -0300

    cookie echo

--- src/test/resources/echo-server/cookie.php
+++ src/test/resources/echo-server/cookie.php
@@ -1,5 +1,20 @@
 <?php
 session_start();
 setcookie("xburger", "opt1");
+
+if(empty($_SESSION['date'])){
+    $_SESSION['date'] = date('H:i:s');
+}
+
+echo "# SESSION:\n";
+foreach ($_SESSION as $key => $value){
+    echo $key . ': ' . $value . "\n";
+}
+
+echo "\n";
+
+echo "# COOKIE:\n";
+foreach ($_COOKIE as $key => $value){
+    echo $key . ': ' . $value . "\n";
+}
 ?>
-OK!
