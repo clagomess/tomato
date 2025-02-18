ba528a9fe40dbbc6f9d55e11914c63d363faa1da
Cláudio Gomes <cla.gomess@gmail.com>
2025-02-18 11:50:14 -0300

    RequestStagingMonitor check currentHash from source

--- src/main/java/com/github/clagomess/tomato/ui/main/request/left/RequestStagingMonitor.java
+++ src/main/java/com/github/clagomess/tomato/ui/main/request/left/RequestStagingMonitor.java
@@ -3,9 +3,14 @@ package com.github.clagomess.tomato.ui.main.request.left;
 import com.github.clagomess.tomato.dto.data.RequestDto;
 import com.github.clagomess.tomato.dto.key.TabKey;
 import com.github.clagomess.tomato.dto.tree.RequestHeadDto;
+import com.github.clagomess.tomato.io.repository.RequestRepository;
 import com.github.clagomess.tomato.publisher.RequestPublisher;
 import com.github.clagomess.tomato.ui.component.StagingMonitor;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+
+@Slf4j
 public class RequestStagingMonitor extends StagingMonitor<RequestDto> {
     private final RequestPublisher requestPublisher = RequestPublisher.getInstance();
 
@@ -21,6 +26,14 @@ public class RequestStagingMonitor extends StagingMonitor<RequestDto> {
 
         if(requestHeadDto == null) {
             this.currentHashCode = 0;
+            return;
+        }
+
+        try {
+            new RequestRepository().load(requestHeadDto)
+                    .ifPresent(item -> this.currentHashCode = item.hashCode());
+        }catch (IOException e){
+            log.error(e.getMessage(), e);
         }
     }
 
