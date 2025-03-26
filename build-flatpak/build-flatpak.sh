ea1ea90f2980e19e2a0898a3ee8a3fb416c80a74
Cláudio Gomes <cla.gomess@gmail.com>
2025-03-26 19:22:13 -0300

    add temurin jre

--- build-flatpak/build-flatpak.sh
+++ build-flatpak/build-flatpak.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # sudo apt install flatpak-builder
-#flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # prepare
 GIT_TAG=$(git describe --tags --abbrev=0 | sed 's/v//')
@@ -22,15 +21,16 @@ cp ../build-linux/icon-64x64.png /tmp/tomato/build-linux/
 cp ../build-linux/icon-128x128.png /tmp/tomato/build-linux/
 
 # copy desktop entry
-cp ../build-linux/tomato.desktop /tmp/tomato/build-linux
+cp ../build-linux/tomato.desktop /tmp/tomato/build-linux/
 echo "Version=${GIT_TAG}" >> /tmp/tomato/build-linux/tomato.desktop
 
-
-flatpak-builder --force-clean --repo=repo build-dir io.github.clagomess.Tomato.yml
-flatpak build-bundle repo tomato-${GIT_TAG}.flatpak io.github.clagomess.Tomato
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo build-dir io.github.clagomess.Tomato.yml
+flatpak build-bundle repo tomato-${GIT_TAG}-x64.flatpak io.github.clagomess.Tomato
 
 # install (Test Only)
 # sudo service dbus start
 
-# flatpak install --user tomato.flatpak
+# flatpak install --user tomato-*.flatpak
 # flatpak run io.github.clagomess.Tomato
+# flatpak remove io.github.clagomess.Tomato
