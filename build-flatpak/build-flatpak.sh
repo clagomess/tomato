ccb312639c0c089ab8177b2aa1da3ad88bd69569
Cláudio Gomes <cla.gomess@gmail.com>
2025-03-27 14:42:16 -0300

    impl. docker build

--- build-flatpak/build-flatpak.sh
+++ build-flatpak/build-flatpak.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-# sudo apt install flatpak-builder
-
-# prepare
-GIT_TAG=$(git describe --tags --abbrev=0 | sed 's/v//')
-
-rm -rf /tmp/tomato
-mkdir -p /tmp/tomato/release
-mkdir -p /tmp/tomato/build-linux
-
-# copy release
-cp -R ../target/release/** /tmp/tomato/release/
+mkdir -p /opt/tomato
 
 # copy executable
-cp ../build-linux/tomato.sh /tmp/tomato/build-linux/
-echo "/app/share/tomato-${GIT_TAG}.jar" >> /tmp/tomato/build-linux/tomato.sh
-
-# copy icons
-cp ../build-linux/icon-64x64.png /tmp/tomato/build-linux/
-cp ../build-linux/icon-128x128.png /tmp/tomato/build-linux/
+echo "/app/share/tomato-${GIT_TAG}.jar" >> /opt/build-linux/tomato.sh
 
 # copy desktop entry
-cp ../build-linux/tomato.desktop /tmp/tomato/build-linux/
-echo "Version=${GIT_TAG}" >> /tmp/tomato/build-linux/tomato.desktop
+echo "Version=${GIT_TAG}" >> /opt/build-linux/tomato.desktop
 
-flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo build-dir io.github.clagomess.Tomato.yml
-flatpak build-bundle repo tomato-${GIT_TAG}-x64.flatpak io.github.clagomess.Tomato
+flatpak-builder --install-deps-from=flathub --repo=repo \
+  build-dir io.github.clagomess.Tomato.yml
+
+flatpak build-bundle repo \
+  tomato-${GIT_TAG}-x64.flatpak \
+  io.github.clagomess.Tomato
+
+cp tomato-${GIT_TAG}-x64.flatpak /opt/result
 
 # install (Test Only)
 # sudo service dbus start
