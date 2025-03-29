087b37154c162fffa271c0c61408d3522f95e84d
Cláudio Gomes <cla.gomess@gmail.com>
2025-03-28 21:19:52 -0300

    add Password Entropy Calculator

---
+++ src/main/java/com/github/clagomess/tomato/io/keepass/PasswordEntropyCalculator.java
@@ -0,0 +1,78 @@
+package com.github.clagomess.tomato.io.keepass;
+
+import lombok.Getter;
+
+import java.math.BigInteger;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class PasswordEntropyCalculator {
+    protected int getSymbolLength(char ch){
+        if(ch < 32) return 32;
+        if(ch >= 48 && ch <= 57) return 10;
+        if(ch >= 65 && ch <= 90) return 26;
+        if(ch >= 97 && ch <= 122) return 26;
+        if(ch <= 127) return 34;
+        if(ch <= 255) return 128;
+
+        return 65279;
+    }
+
+    protected Set<Character> getUniqueSymbols(String password){
+        if(password == null) return Set.of();
+
+        Set<Character> result = new TreeSet<>();
+
+        for(char ch : password.toCharArray()){
+            result.add(ch);
+        }
+
+        return result;
+    }
+
+    protected Entropy calculateEntropy(String password){
+        Set<Character> uniqueSymbols = getUniqueSymbols(password);
+        if(uniqueSymbols.isEmpty()) return new Entropy(0, 0, 0);
+
+        int symbolLength = 0;
+
+        int currentLength = 0;
+        for(char ch : uniqueSymbols){
+            currentLength = getSymbolLength(ch);
+            if(currentLength > symbolLength){
+                symbolLength = currentLength;
+            }
+        }
+
+        int bits = BigInteger.valueOf(currentLength)
+                .pow(uniqueSymbols.size())
+                .bitLength();
+
+        return new Entropy(password.length(), uniqueSymbols.size(), bits);
+    }
+
+    public enum EntropyStrength {
+        LOW, MEDIUM, HIGH
+    }
+
+    @Getter
+    public static class Entropy {
+        private final int charLength;
+        private final int uniqueCharLength;
+        private final int bits;
+        private final EntropyStrength strength;
+
+        public Entropy(int charLength, int uniqueCharLength, int bits) {
+            this.charLength = charLength;
+            this.uniqueCharLength = uniqueCharLength;
+            this.bits = bits;
+            this.strength = calculateStrength();
+        }
+
+        private EntropyStrength calculateStrength(){
+            if(bits < 45) return EntropyStrength.LOW;
+            if(bits < 80) return EntropyStrength.MEDIUM;
+            return EntropyStrength.HIGH;
+        }
+    }
+}
---
+++ src/test/java/com/github/clagomess/tomato/io/keepass/PasswordEntropyCalculatorTest.java
@@ -0,0 +1,68 @@
+package com.github.clagomess.tomato.io.keepass;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PasswordEntropyCalculatorTest {
+    private final PasswordEntropyCalculator calculator = new PasswordEntropyCalculator();
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "\r,32",
+            "1,10",
+            "a,26",
+            "A,26",
+            "@,34",
+            "ç,128",
+            "ご,65279",
+            "😂,65279",
+    }, ignoreLeadingAndTrailingWhitespace = false)
+    public void getSymbolLength(String ch, int expected) {
+        assertEquals(
+                expected,
+                calculator.getSymbolLength(ch.charAt(0))
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            ",0",
+            "a,1",
+            "aaa,1",
+            "aaa1,2",
+            "ごご,1",
+            "😂,2",
+    })
+    public void getUniqueSymbols(String password, int expected){
+        assertEquals(
+                expected,
+                calculator.getUniqueSymbols(password).size()
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            ",0,LOW",
+            "123456,20,LOW",
+            "password,33,LOW",
+            "qwerty,29,LOW",
+            "aaaaaaaa,5,LOW",
+            "00000000,4,LOW",
+            "iF10!6,29,LOW",
+            "$xZ6@Mg115f,48,MEDIUM",
+            "😂y%a#laHgç~,176,HIGH",
+            "resuITrecetreprOfolYcefAnCeIStBowkNisTaw,113,HIGH",
+    })
+    public void calculateEntropy(
+            String password,
+            int expectedBits,
+            PasswordEntropyCalculator.EntropyStrength expectedStrength
+    ){
+        var result = calculator.calculateEntropy(password);
+
+        assertEquals(expectedBits, result.getBits());
+        assertEquals(expectedStrength, result.getStrength());
+    }
+}
