6222dea70d812dbe0f83c21faa5cb85677e880e9
Cláudio Gomes <cla.gomess@gmail.com>
2025-05-30 16:28:42 -0300

    build-debian: created image

--- build-debian/build-debian.sh
+++ build-debian/build-debian.sh
@@ -1,44 +1,19 @@
 #!/bin/bash
 
-# prepare
-GIT_TAG=$(git describe --tags --abbrev=0 | sed 's/v//')
-
-rm -rf /tmp/tomato**
-mkdir -p /tmp/tomato/DEBIAN
-mkdir -p /tmp/tomato/usr/share/doc/tomato
-mkdir -p /tmp/tomato/usr/share/tomato
-mkdir -p /tmp/tomato/usr/bin
+DEBIAN_TARGET="/opt/build-debian/tomato"
 
 # copy release
-cp -R ../target/release/** /tmp/tomato/usr/share/tomato
+cp -R /opt/release/** "${DEBIAN_TARGET}/usr/share/tomato/"
 
 # copy executable
-cp ../build-linux/tomato.sh /tmp/tomato/usr/bin/tomato
-echo "/usr/share/tomato/tomato-${GIT_TAG}.jar" >> /tmp/tomato/usr/bin/tomato
-chmod +x /tmp/tomato/usr/bin/tomato
-
-# copy icons
-mkdir -p /tmp/tomato/usr/share/icons/hicolor/64x64/apps
-mkdir -p /tmp/tomato/usr/share/icons/hicolor/128x128/apps
-cp ../build-linux/icon-64x64.png /tmp/tomato/usr/share/icons/hicolor/64x64/apps/tomato.png
-cp ../build-linux/icon-128x128.png /tmp/tomato/usr/share/icons/hicolor/128x128/apps/tomato.png
-
-# copy desktop entry
-mkdir -p /tmp/tomato/usr/share/applications
-cp ../build-linux/tomato.desktop /tmp/tomato/usr/share/applications/tomato.desktop
-echo "Version=${GIT_TAG}" >> /tmp/tomato/usr/share/applications/tomato.desktop
+echo "/usr/share/tomato/tomato-${GIT_TAG}.jar" >> "${DEBIAN_TARGET}/usr/bin/tomato"
 
 # copy control
-cp control /tmp/tomato/DEBIAN/
-echo "Version: ${GIT_TAG}" >> /tmp/tomato/DEBIAN/control
-chmod -R 755 /tmp/tomato/DEBIAN
-
-# copy copyright
-cp copyright /tmp/tomato/usr/share/doc/tomato/
+echo "Version: ${GIT_TAG}" >> "${DEBIAN_TARGET}/DEBIAN/control"
 
 # build
-dpkg-deb --build /tmp/tomato
-mv /tmp/tomato.deb "/tmp/tomato-${GIT_TAG}-all.deb"
+dpkg-deb --build ${DEBIAN_TARGET}
+cp /opt/build-debian/tomato.deb /opt/result/tomato-${GIT_TAG}-all.deb
 
 # install (Test Only)
-# sudo apt install /tmp/tomato.deb -y
+# sudo apt install ./tomato-0.0.0-all.deb -y
