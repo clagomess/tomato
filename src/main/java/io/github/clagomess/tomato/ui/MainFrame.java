9960ec5cfac4bc952f00ec16b7c79a7d9c9d9cc7
Claudio Gomes <cla.gomess@gmail.com>
2026-03-25 18:03:41 -0300

    Add macOS-specific application lifecycle handlers and enhancements

--- src/main/java/io/github/clagomess/tomato/ui/MainFrame.java
+++ src/main/java/io/github/clagomess/tomato/ui/MainFrame.java
@@ -8,11 +8,14 @@ import io.github.clagomess.tomato.ui.menu.CollectionMenu;
 import io.github.clagomess.tomato.ui.menu.EnvironmentMenu;
 import io.github.clagomess.tomato.ui.menu.SettingsMenu;
 import io.github.clagomess.tomato.ui.menu.WorkspaceMenu;
+import io.github.clagomess.tomato.ui.settings.AboutFrame;
+import io.github.clagomess.tomato.ui.settings.ConfigurationFrame;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
 
+import static io.github.clagomess.tomato.ui.component.PreventDefaultFrame.toFrontIfExists;
 import static javax.swing.SwingUtilities.invokeLater;
 
 public class MainFrame extends JFrame {
@@ -25,10 +28,39 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new MainWindowAdapter(this));
 
-        if (SystemInfo.isMacFullWindowContentSupported) {
-            getRootPane().putClientProperty("apple.awt.transparentTitleBar", true);
-            getRootPane().putClientProperty("apple.awt.fullWindowContent", true);
-            getRootPane().putClientProperty("apple.awt.windowTitleVisible", false);
+        if(SystemInfo.isMacOS) {
+            if (SystemInfo.isMacFullWindowContentSupported) {
+                getRootPane().putClientProperty("apple.awt.transparentTitleBar", true);
+                getRootPane().putClientProperty("apple.awt.fullWindowContent", true);
+                getRootPane().putClientProperty("apple.awt.windowTitleVisible", false);
+            }
+
+            var desktop = Desktop.getDesktop();
+
+            if (desktop.isSupported(Desktop.Action.APP_ABOUT)) {
+                desktop.setAboutHandler(e -> toFrontIfExists(
+                        AboutFrame.class,
+                        () -> new AboutFrame(this)
+                ));
+            }
+
+            if (desktop.isSupported(Desktop.Action.APP_PREFERENCES)) {
+                desktop.setPreferencesHandler(e -> toFrontIfExists(
+                        ConfigurationFrame.class,
+                        () -> new ConfigurationFrame(this)
+                ));
+            }
+
+            if (desktop.isSupported(Desktop.Action.APP_QUIT_HANDLER)) {
+                desktop.setQuitHandler((e, response) -> {
+                    if(MainWindowAdapter.shouldQuit(this)){
+                        this.dispose();
+                        response.performQuit();
+                    }else{
+                        response.cancelQuit();
+                    }
+                });
+            }
         }
 
         setLayout(new MigLayout("insets 5", "[grow, fill]"));
