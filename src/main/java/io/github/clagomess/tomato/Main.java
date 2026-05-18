992b3509830ab06a160d4c0762b8b2a2acf1c3e1
Claudio Gomes <cla.gomess@gmail.com>
2026-05-18 14:47:19 -0300

    Remove MainSingleInstance class and associated single instance management logic

--- src/main/java/io/github/clagomess/tomato/Main.java
+++ src/main/java/io/github/clagomess/tomato/Main.java
@@ -3,18 +3,14 @@ package io.github.clagomess.tomato;
 import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.util.SystemInfo;
 import io.github.clagomess.tomato.ui.MainFrame;
-import io.github.clagomess.tomato.ui.MainSingleInstance;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.swing.*;
 
 @Slf4j
 public class Main {
-    private static final MainSingleInstance singleInstance = new MainSingleInstance();
-
     public static void main(String[] argv){
         log.info("Starting...");
-        if(singleInstance.alreadyStarted()) return;
 
         if (SystemInfo.isMacOS) {
             System.setProperty("apple.laf.useScreenMenuBar", "true");
@@ -25,11 +21,6 @@ public class Main {
         FlatDarculaLaf.registerCustomDefaultsSource( "io.github.clagomess.tomato.ui" );
         FlatDarculaLaf.setup();
 
-        SwingUtilities.invokeLater(() -> {
-            var mainFrame = new MainFrame();
-            singleInstance.setBringToFront(() ->
-                    SwingUtilities.invokeLater(mainFrame::toFront)
-            );
-        });
+        SwingUtilities.invokeLater(MainFrame::new);
     }
 }
