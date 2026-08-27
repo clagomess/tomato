0b93de51485551298c02fd63d13b847af9995e59
Claudio Gomes <cla.gomess@gmail.com>
2026-08-27 11:00:33 -0300

    Resolve and validate collectionFileDir paths in RequestFileChooser

--- src/main/java/io/github/clagomess/tomato/ui/component/RequestFileChooser.java
+++ src/main/java/io/github/clagomess/tomato/ui/component/RequestFileChooser.java
@@ -10,12 +10,17 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.AccessDeniedException;
 
+import static com.formdev.flatlaf.FlatClientProperties.STYLE;
 import static io.github.clagomess.tomato.io.repository.EnvironmentRepository.SYSENV_COLLECTION_FILE_DIR_KEY;
 import static io.github.clagomess.tomato.util.FileUtils.COLLECTION_FILES_DIR;
 import static javax.swing.JFileChooser.FILES_ONLY;
 
 @Slf4j
 public class RequestFileChooser extends FileChooser {
+    private static final String DEFAULT_STYLE = "";
+    private static final String SUCCESS_STYLE = "foreground: #75BA24";
+    private static final String ERROR_STYLE = "outline: error; foreground: #FF5555";
+
     private static final String DIALOG_TITLE = "Import File";
     private static final String DIALOG_MESSAGE = "Do you want to import this file to collection directory?";
     private final RequestHeadDto requestHead;
@@ -23,8 +28,23 @@ public class RequestFileChooser extends FileChooser {
     public RequestFileChooser(@Nullable RequestHeadDto requestHead) {
         super(FILES_ONLY);
         this.requestHead = requestHead;
-    }
 
+        addOnChange(text -> {
+            var value = getValue();
+
+            if(value != null && text.contains(SYSENV_COLLECTION_FILE_DIR_KEY) && value.exists()){
+                putClientProperty(STYLE, SUCCESS_STYLE);
+                return;
+            }
+
+            if(value != null && !value.exists()){
+                putClientProperty(STYLE, ERROR_STYLE);
+                return;
+            }
+
+            putClientProperty(STYLE, DEFAULT_STYLE);
+        });
+    }
 
     @Override
     protected void btnSelectAction(){
@@ -32,9 +52,27 @@ public class RequestFileChooser extends FileChooser {
         new WaitExecution(this, btnSelect, this::importFile).execute();
     }
 
+    @Override
+    public @Nullable File getValue(){
+        var text = getText();
+        if(StringUtils.isBlank(text)) return null;
+
+        if(!text.contains(SYSENV_COLLECTION_FILE_DIR_KEY)){
+            return new File(text);
+        }
+
+        if(requestHead == null) return null;
+
+        return new File(text.replace(
+                "{{" + SYSENV_COLLECTION_FILE_DIR_KEY + "}}",
+                requestHead.getPath().getParent() + "/" + COLLECTION_FILES_DIR
+        ));
+    }
+
     public void importFile() throws IOException {
         if(requestHead == null) return;
         if(StringUtils.isBlank(getText())) return;
+        if(getText().contains(SYSENV_COLLECTION_FILE_DIR_KEY)) return;
 
         int ret = JOptionPane.showConfirmDialog(
                 this,
